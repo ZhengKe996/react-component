8b16f3d368237874c670936c8d35d4134a76e2ee
ZhengKe <zhengke996@163.com>
2022-09-30 18:23:05 +0800

    fix: change test

--- src/Tag/index.test.tsx
+++ src/Tag/index.test.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
+import Tag from './index'
 describe('Button', () => {
-  test('renders Button', () => {})
+  test('renders Tag', () => {
+    render(<Tag>click me</Tag>)
+    const linkElement = screen.getByText(/click me/i)
+    expect(linkElement).toBeInTheDocument()
+  })
 })
